b6c5425886e6164bb627268c171ba1a1c6b9ea07
Maximiliano Ustria <maxiustria@192.168.1.11>
2021-11-16 19:49:02 -0300

    info meli

--- meli/request.py
+++ meli/request.py
@@ -1,5 +1,39 @@
 import requests
+import sys
+import json
+
+barrio = sys.argv
 
 r = requests.get(
-    'https://api.mercadolibre.com/sites/MLU/search?category=MLU6393&q=%punta%20ballena%')
-print(r.text)
+    "https://api.mercadolibre.com/sites/MLU/search?category=MLU6393&q={}".format(barrio))
+apartamentos = []
+for apto in json.loads(r.text)['results']:
+    apartamento = {
+        "id": apto["id"],
+        "nombre": apto['title'],
+        "precio": apto['price'],
+        "moneda": apto['currency_id'],
+        "dirección": apto['location']['address_line'],
+        "foto": apto['thumbnail'],
+        "area_total": "",
+        "tipo_de_inmueble": "",
+        "baños": "",
+        "area_cubierta": "",
+        "dormitorios": "",
+        "estado_apto": ""
+    }
+    for info in apto['attributes']:
+        valor = info['value_name']
+        if apto['id'] == 'ITEM_CONDITION':
+            apartamento['estado_apto'] = valor
+        elif apto['id'] == 'BEDROOMS':
+            apartamento['dormitorios'] = valor
+        elif apto['id'] == 'COVERED_AREA':
+            apartamento['area_cubierta'] = valor
+        elif apto['id'] == 'FULL_BATHROOMS':
+            apartamento['baños'] = valor
+        elif apto['id'] == 'PROPERTY_TYPE':
+            apartamento['tipo_de_inmueble'] = valor
+        elif apto['id'] == 'TOTAL_AREA':
+            apartamento['area_total'] = valor
+    apartamentos.append(apartamento)
